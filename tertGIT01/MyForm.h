e10071c2981245601aac9fe3cb79790b9d85be9c
Robert <robert@laptop.pc>
2015-10-08 02:01:01 +0200

    commit

---
+++ tertGIT01/MyForm.h
@@ -0,0 +1,80 @@
+#pragma once
+
+namespace Projekt4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyœæ wszystkie u¿ywane zasoby.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::ListBox^  listBox1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Wymagana metoda wsparcia projektanta - nie nale¿y modyfikowaæ
+		/// zawartoœæ tej metody z edytorem kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(66, 33);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(120, 95);
+			this->listBox1->TabIndex = 0;
+			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::listBox1_SelectedIndexChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->listBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void listBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	};
+}
