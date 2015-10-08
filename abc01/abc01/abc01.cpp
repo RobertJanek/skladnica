2ec6900aac8135373b94828fb41a50f9653ce54f
Robert <robert@laptop.pc>
2015-10-08 02:10:01 +0200

    przekazanie01testowe

---
+++ abc01/abc01/abc01.cpp
@@ -0,0 +1,13 @@
+// abc01.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "jak to wypchn¹æ na GitHuba";
+	return 0;
+}
+
